5f06f64442364c866491edea83ea7b495fe7b1ea
aBadMan <114421846+aBadMan91@users.noreply.github.com>
2025-10-06 12:42:38 +0200

    Small changes
    - Changed some image names
    
    - Various small changes

--- src/layouts/LayoutMain/index.jsx
+++ src/layouts/LayoutMain/index.jsx
@@ -6,7 +6,7 @@ export default function LayoutMain() {
   return (
     <div className="flex flex-col min-h-svh text-white" style={{ backgroundColor: "var(--color-secondary)" }}>
       <Header />
-      <main className="flex-1 container mx-auto px-4 py-8">
+      <main className="flex-1 container mx-auto px-4 pt-16 pb-8">
         <Outlet />
       </main>
       <Footer />
--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -8,8 +8,7 @@ export default function Home() {
 
   return (
     <div>
-      <h1>Welcome to Gamehub</h1>
-      <p>Your one-stop destination for all things gaming.</p>
+      <h1>Highlighted Game</h1>
       <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4">
         <Card image="/src/assets/images/game1.jpg" title="Game 1" price="$29.99" />
         <Card image="/src/assets/images/game2.jpg" title="Game 2" price="$39.99" />
